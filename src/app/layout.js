2279352507e822bf73aca424b4586589eee345fb
benhuynh003 <155077782+benhuynh003@users.noreply.github.com>
2024-10-26 09:54:51 -0700

    layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,14 +2,16 @@ import "./globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 
 export const metadata = {
-  title: "Dress to Impress :3",
+  title: "Cosmos Explorer",
   description: "DA Hacks 3.0 @ 2024",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className="antialiased">
+        <NextUIProvider>{children}</NextUIProvider>
+      </body>
     </html>
   );
 }
